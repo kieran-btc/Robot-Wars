41e836811f59f0c7ed500d2938fe0eb7fc7d3522
Beißel <kieran.beissel@btc-it-services.com>
2024-09-24 12:20:54 +0200

    Hinzugefügt: Check auf unerlaubte Zeichen im Namen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,64 +24,144 @@ public class Main {
         TimeUnit.MILLISECONDS.sleep(100);
         System.out.println("        _\\///________\\///_______\\/////_______\\/////////////_________\\/////_____________\\///___________________________\\///____\\///_______\\///________\\///__\\///________\\///____\\///////////_____");
 
-// Avatar-Name anlegen
+// Avatar-Name anlegen und check auf unerlaubte Zeichen
 
         String avatar;
-        int bestätigung;
         Scanner scan1 = new Scanner(System.in);
-        Scanner scan2 = new Scanner(System.in);
+        boolean unerlaubt = true;
 
-        System.out.println("Bitte gib den Namen deines Avatars ein: ");
-        avatar = scan1.nextLine();
-        System.out.println("Der Name deines Avatars lautet: " + avatar + " Bestätigen (1) oder neu eingeben (2)");
-        bestätigung = scan2.nextInt();
+//        while (bestätigung != 1) {
+//
+//            if (bestätigung == 2) {
+//                System.out.println("Bitte gib den Namen deines Avatars ein: ");
+//                avatar = scan1.nextLine();
+//                System.out.println("Der Name deines Avatars lautet: " + avatar + " Bestätigen (1) oder neu eingeben (2)");
+//                bestätigung = scan2.nextInt();
+//            } else {
+//                System.out.println("Ungültig");
+//                bestätigung = 2;
+//            }
+//        }
 
-        while (bestätigung != 1)
+            System.out.println("Bitte gib den Namen deines Avatars ein: ");
+            avatar = scan1.nextLine();
 
-            if (bestätigung == 2) {
-                System.out.println("Bitte gib den Namen deines Avatars ein: ");
-                avatar = scan1.nextLine();
-                System.out.println("Der Name deines Avatars lautet: " + avatar + " Bestätigen (1) oder neu eingeben (2)");
-                bestätigung = scan2.nextInt();
-            } else {
-                System.out.println("Ungültig");
-                bestätigung = 2;
+            while (unerlaubt) {
+                if (avatar.contains("^")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("°")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("!")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("²")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("§")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("³")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("$")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("%")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("&")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("/")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("{")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("(")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("[")) {
+                    unerlaubt = true;
+                } else if (avatar.contains(")")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("]")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("=")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("}")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("?")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("\\")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("´")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("`")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("+")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("*")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("#")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("'")) {
+                    unerlaubt = true;
+                } else if (avatar.contains(":")) {
+                    unerlaubt = true;
+                } else if (avatar.contains(";")) {
+                    unerlaubt = true;
+                } else if (avatar.contains(",")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("<")) {
+                    unerlaubt = true;
+                } else if (avatar.contains(">")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("|")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("@")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("€")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("µ")) {
+                    unerlaubt = true;
+                } else if (avatar.contains("\"")) {
+                    unerlaubt = true;
+                } else if (avatar.contains(" ")) {
+                    unerlaubt = true;
+                } else {
+                    unerlaubt = false;
+                }
+                if (unerlaubt) {
+                    System.out.println("Der Name " + avatar + " enthält ungültige Zeichen. Bitte erneut eingeben: ");
+                    avatar = scan1.nextLine();
+                }
             }
 
-        System.out.println("Der Name wurde gespeichert!");
+            System.out.println("Dein Name wurde gespeichert! Er lautet: " + avatar);
 
 // Spielfeld anlegen mit vorher abgefragter Roboterposition
 
-        Scanner scan3 = new Scanner(System.in);
-        Scanner scan4 = new Scanner(System.in);
+            Scanner scan3 = new Scanner(System.in);
+            Scanner scan4 = new Scanner(System.in);
 
-        System.out.println("Auf welcher X-Position (1-15) möchtest du mit deinem Roboter starten?");
-        int robox = scan3.nextInt();
-        System.out.println("Auf welcher Y-Position (1-10) möchtest du mit deinem Roboter starten?");
-        int roboy = scan4.nextInt();
+            System.out.println("Auf welcher X-Position (1-15) möchtest du mit deinem Roboter starten?");
+            int robox = scan3.nextInt();
+            System.out.println("Auf welcher Y-Position (1-10) möchtest du mit deinem Roboter starten?");
+            int roboy = scan4.nextInt();
 
-        int x = 1;
-        int y = 1;
+            int x = 1;
+            int y = 1;
 
-        while (y <= 10) {            // Höhe 10
-            x = 1;
-            while (x <= 15) {        // Breite 15
-                if (x == robox && y == roboy) {
-                    System.out.print("{Ö}");
-                    x++;
-                } else {
-                    System.out.print("[ ]");
-                    x++;
-                    TimeUnit.MILLISECONDS.sleep(10);
+            while (y <= 10) {            // Höhe 10
+                x = 1;
+                while (x <= 15) {        // Breite 15
+                    if (x == robox && y == roboy) {
+                        System.out.print("{Ö}");
+                        x++;
+                    } else {
+                        System.out.print("[ ]");
+                        x++;
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    }
                 }
+                System.out.println();
+                y++;
+                TimeUnit.MILLISECONDS.sleep(10);
             }
-            System.out.println();
-            y++;
-            TimeUnit.MILLISECONDS.sleep(10);
-        }
 
 // Roboterposition ausgeben
 
-        System.out.println("Der Roboter befindet sich in X=" + robox + " Y=" + roboy);
+            System.out.println("Der Roboter befindet sich in X=" + robox + " Y=" + roboy);
+        }
     }
-}
